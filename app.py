c2259284212ab398af37ce25079f46a089ecebba
Rahul Jain <114798084+Rahul-Jain-1@users.noreply.github.com>
2024-03-26 00:12:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,11 +18,11 @@ text_model = tf.keras.models.model_from_json(loaded_model_json_1)
 text_model.load_weights('model_weights_text.h5')
 
 
-# Load the audio model
-with open('model.json', 'r') as json_file:
-    loaded_model_json_2 = json_file.read()
-audio_model = tf.keras.models.model_from_json(loaded_model_json_2)
-audio_model.load_weights("model_weights.h5")
+# # Load the audio model
+# with open('model.json', 'r') as json_file:
+#     loaded_model_json_2 = json_file.read()
+# audio_model = tf.keras.models.model_from_json(loaded_model_json_2)
+# audio_model.load_weights("model_weights.h5")
 
 
 # Load the tokenizer
@@ -30,6 +30,9 @@ with open('tokenizer1.json') as f:
     data = json.load(f)
     tokenizer = tf.keras.preprocessing.text.tokenizer_from_json(data)
 
+@app.route('/', methods=['GET'])
+def main():
+    print("hello :")
 # Prediction route
 @app.route('/predict_text', methods=['GET', 'POST'])
 def predict_text():
@@ -56,25 +59,25 @@ def predict_text():
     # Return a default response for GET requests
     return jsonify({'error': 'Invalid request method'}), 400
 
-# Define a route for making predictions
-@app.route('/predict_audio', methods=['GET', 'POST'])
-def predict_audio():
+# # Define a route for making predictions
+# @app.route('/predict_audio', methods=['GET', 'POST'])
+# def predict_audio():
    
-     file = request.files['file']
+#      file = request.files['file']
     
-     if file:
-        sound_signal, sample_rate = librosa.load(file, sr=None, res_type="kaiser_fast")  # Add sr=None
-        mfcc_features = librosa.feature.mfcc(y=sound_signal, sr=sample_rate, n_mfcc=40)
-        mfccs_features_scaled = np.mean(mfcc_features.T, axis=0)
-        mfccs_features_scaled = mfccs_features_scaled.reshape(1, -1)
-        result_array = audio_model.predict(mfccs_features_scaled)
-        print(result_array)
-        result_classes = ["REAL", "FAKE"]
-        result = np.argmax(result_array[0])
-        print("Result:", result_classes[result])
-        return jsonify({'predictions':  result_classes[result]})
-     else:
-        return "Invalid file"
+#      if file:
+#         sound_signal, sample_rate = librosa.load(file, sr=None, res_type="kaiser_fast")  # Add sr=None
+#         mfcc_features = librosa.feature.mfcc(y=sound_signal, sr=sample_rate, n_mfcc=40)
+#         mfccs_features_scaled = np.mean(mfcc_features.T, axis=0)
+#         mfccs_features_scaled = mfccs_features_scaled.reshape(1, -1)
+#         result_array = audio_model.predict(mfccs_features_scaled)
+#         print(result_array)
+#         result_classes = ["REAL", "FAKE"]
+#         result = np.argmax(result_array[0])
+#         print("Result:", result_classes[result])
+#         return jsonify({'predictions':  result_classes[result]})
+#      else:
+#         return "Invalid file"
 
 
 if __name__ == '__main__':
